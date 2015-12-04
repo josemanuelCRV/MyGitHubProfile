3a3e09f58454363a6f5f3d155f10bce2145456f8
JoseManuel <josemanuelcrv@gmail.com>
2015-12-04 19:59:51 +0100

    Commit#2: Added controller class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,84 @@
 package application;
-	
+
+import java.io.File;
+import java.io.IOException;
+
+import control_pck.MyGitHubProfile_controller;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
+
+	private Stage primaryStage;
+	private AnchorPane MyGitHubProfile_view;
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+		// Constructor vacio
+	}
+
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root, 400, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			this.primaryStage = primaryStage;
+			this.primaryStage.setTitle("MyGitHub Profile");
 			primaryStage.show();
-		} catch(Exception e) {
+			initMyGitHubProfile_view();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	private void initMyGitHubProfile_view() {
+		// TODO Auto-generated method stub
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("../vista_pck/MyGitHubProfile_view.fxml"));
+			MyGitHubProfile_view = (AnchorPane) loader.load();
+
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(MyGitHubProfile_view);
+			primaryStage.setScene(scene);
+
+			// Give the controller access to the main app.
+			MyGitHubProfile_controller controller = loader.getController();
+			controller.setMain(this);
+
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+
+
+	}
+
+
+
+
+	/**
+	 * Method getPrimaryStage
+	 *
+	 * @return the primaryStage
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+
+	}
+
+
+
 	public static void main(String[] args) {
 		launch(args);
 	}
---
+++ src/control_pck/MyGitHubProfile_controller.java
@@ -0,0 +1,18 @@
+package control_pck;
+
+import application.Main;
+
+public class MyGitHubProfile_controller {
+
+	public void setMain(Main main) {
+		// TODO Auto-generated method stub
+
+
+	}
+
+
+
+
+
+
+}
